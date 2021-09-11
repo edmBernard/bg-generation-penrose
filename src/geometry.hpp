437d3fdd5a6ca9e0763c1a87b0197a3cfac8a88a
edmbernard <bernard.erwan@gmail.com>
2021-09-11 16:51:50 +0200

    fix: approximate comparison between point

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -7,37 +7,42 @@
 #include <optional>
 #include <string>
 
+constexpr float epsilon = 0.1f; // use for the comparison between Point
+
 struct Point {
   float x;
   float y;
 };
 
-bool operator==(const Point& lhs, const Point& rhs) {
-  return lhs.x == rhs.x && lhs.y == rhs.y;
-}
-bool operator<(const Point& lhs, const Point& rhs) {
-  if (lhs.x == rhs.x)
-    return lhs.y < rhs.y;
-  return lhs.x < rhs.x;
-}
 
-Point operator+(Point pt1, Point pt2) {
+Point operator+(const Point &pt1, const Point & pt2) {
   return {pt1.x + pt2.x, pt1.y + pt2.y};
 }
-Point operator-(Point pt1, Point pt2) {
+Point operator-(const Point & pt1, const Point & pt2) {
   return {pt1.x - pt2.x, pt1.y - pt2.y};
 }
-Point operator*(float value, Point pt) {
+Point operator*(float value, const Point & pt) {
   return {value * pt.x, value * pt.y};
 }
-Point operator*(Point pt, float value) {
+Point operator*(const Point & pt, float value) {
   return {value * pt.x, value * pt.y};
 }
-Point operator/(Point pt, float value) {
+Point operator/(const Point & pt, float value) {
   return {pt.x / value, pt.y / value};
 }
-float scalar(Point pt1, Point pt2) {
+float scalar(const Point & pt1, const Point & pt2) {
   return pt1.x * pt2.x + pt1.y * pt2.y;
+}
+float norm(const Point & pt1) {
+  return pt1.x * pt1.x + pt1.y * pt1.y;
+}
+bool operator==(const Point &lhs, const Point &rhs) {
+  return norm(lhs - rhs) < epsilon;
+}
+bool operator<(const Point &lhs, const Point &rhs) {
+  if (abs(lhs.x - rhs.x) < epsilon)
+    return lhs.y < rhs.y;
+  return lhs.x < rhs.x;
 }
 
 enum Color {
@@ -57,9 +62,13 @@ struct Triangle {
     vertices[1] = B;
     vertices[2] = C;
   }
+
+  Point center() {
+    return (this->vertices[0] + this->vertices[1] + this->vertices[2]) / 3.;
+  }
 };
 
-bool operator==(const Triangle& lhs, const Triangle& rhs) {
+bool operator==(const Triangle &lhs, const Triangle &rhs) {
   return rhs.vertices[0] == lhs.vertices[0] &&
          rhs.vertices[1] == lhs.vertices[1] &&
          rhs.vertices[2] == lhs.vertices[2];
@@ -74,23 +83,20 @@ struct Quadrilateral {
     vertices[2] = C;
     vertices[3] = D;
   }
+
+  Point center() const {
+    return (this->vertices[0] + this->vertices[1] + this->vertices[2] + this->vertices[3]) / 4.;
+  }
 };
 
-bool operator==(const Quadrilateral& lhs, const Quadrilateral& rhs) {
-  return rhs.vertices[0] == lhs.vertices[0] &&
-         rhs.vertices[1] == lhs.vertices[1] &&
-         rhs.vertices[2] == lhs.vertices[2] &&
-         rhs.vertices[3] == lhs.vertices[3];
+bool operator==(const Quadrilateral &lhs, const Quadrilateral &rhs) {
+  // we compare gravity center approximative be enough
+  return lhs.center() == rhs.center();
 }
 
-bool operator<(const Quadrilateral& lhs, const Quadrilateral& rhs) {
-  if (rhs.vertices[0] == lhs.vertices[0])
-    if (rhs.vertices[1] == lhs.vertices[1])
-      if (rhs.vertices[2] == lhs.vertices[2])
-        return rhs.vertices[3] == lhs.vertices[3];
-      return rhs.vertices[2] < lhs.vertices[2];
-    return rhs.vertices[1] < lhs.vertices[1];
-  return rhs.vertices[0] < lhs.vertices[0];
+bool operator<(const Quadrilateral &lhs, const Quadrilateral &rhs) {
+  // we compare gravity center approximative be enough
+  return lhs.center() < rhs.center();
 }
 
 std::string to_string(const Point &pt) {
