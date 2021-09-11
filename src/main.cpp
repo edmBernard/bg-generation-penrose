cfcc225d10559ddb997e9a2bf785c86cc9c9cf0a
edmbernard <bernard.erwan@gmail.com>
2021-09-11 12:28:34 +0200

    feat: add first version with path

--- src/main.cpp
+++ src/main.cpp
@@ -76,6 +76,14 @@ int main(int argc, char *argv[]) try {
   std::chrono::duration<double, std::milli> elapsed_temp = std::chrono::high_resolution_clock::now() - start_temp;
   fmt::print("Time to save svg: {:.2f} ms \n", elapsed_temp.count());
 
+  auto start_temp2 = std::chrono::high_resolution_clock::now();
+  if (!saveTiling_v2(tiling, canvasSize)) {
+    spdlog::error("Failed to save in file");
+    return EXIT_FAILURE;
+  }
+  std::chrono::duration<double, std::milli> elapsed_temp2 = std::chrono::high_resolution_clock::now() - start_temp2;
+  fmt::print("Time to save svg: {:.2f} ms \n", elapsed_temp2.count());
+
   return EXIT_SUCCESS;
 
 } catch (const cxxopts::OptionException &e) {
--- src/save.hpp
+++ src/save.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <penrose.hpp>
+
+#include <spdlog/spdlog.h>
+
+#include <fstream>
 #include <vector>
 
 [[nodiscard]] bool saveTiling(const std::vector<penrose::PenroseTriangle> &triangles, int canvasSize) {
 
   std::ofstream out("penrose_tiling.svg");
   if (!out) {
-    std::cerr << "Cannot open output file.\n";
+    spdlog::error("Cannot open output file.");
     return false;
   }
 
@@ -24,6 +28,36 @@
     // out << fmt::format("<line x1='{:.3f}' y1='{:.3f}' x2='{:.3f}' y2='{:.3f}'/>\n", tr.vertices[1].x, tr.vertices[1].y, tr.vertices[2].x, tr.vertices[2].y);
     out << fmt::format("<line x1='{:.3f}' y1='{:.3f}' x2='{:.3f}' y2='{:.3f}'/>\n", tr.vertices[2].x, tr.vertices[2].y, tr.vertices[0].x, tr.vertices[0].y);
   }
+  out << "</g>\n</svg>\n";
+  return true;
+}
+
+[[nodiscard]] bool saveTiling_v2(const std::vector<penrose::PenroseTriangle> &triangles, int canvasSize) {
+
+  std::ofstream out("penrose_tiling_v2.svg");
+  if (!out) {
+    spdlog::error("Cannot open output file.");
+    return false;
+  }
+
+  out << "<svg xmlns='http://www.w3.org/2000/svg' height='" << canvasSize
+      << "' width='" << canvasSize << "'>\n"
+      << "<rect height='100%' width='100%' fill='black'/>\n"
+      << "<g id='surface1'>";
+  out << "<path style='stroke:none;fill-rule:nonzero;fill:rgb(54.901961%,54.901961%,54.901961%);fill-opacity:1;'"
+      << " d='";
+  for (const auto &tr : triangles) {
+    out << fmt::format("M {} {} L {} {} L {} {} Z ", tr.vertices[2].x, tr.vertices[2].y, tr.vertices[0].x, tr.vertices[0].y, tr.vertices[1].x, tr.vertices[1].y);
+  }
+  out << "'> </path>\n";
+
+
+  out << "<path style='fill:none;stroke-width:0.0412023;stroke-linecap:butt;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;'"
+      << " d='";
+  for (const auto &tr : triangles) {
+    out << fmt::format("M {} {} L {} {} L {} {} Z ", tr.vertices[2].x, tr.vertices[2].y, tr.vertices[0].x, tr.vertices[0].y, tr.vertices[1].x, tr.vertices[1].y);
+  }
+  out << "'> </path>\n";
   out << "</g>\n</svg>\n";
   return true;
 }
